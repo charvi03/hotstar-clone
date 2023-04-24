a82ebf9bb5dcd3c2c52410edffb6fa0191d1d57a
charvi03 <charvikhurana01@gmail.com>
2023-04-24 22:03:25 +0530

    init

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth, provider } from "../firebase";
 import {
@@ -14,15 +15,34 @@ const Header = (props) => {
   const userPhoto = useSelector(selectUserPhoto);
   const history = useNavigate();
 
+  useEffect(() => {
+    auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        setUser(user);
+        history.push("/home");
+      }
+    });
+  }, [userName]);
+
   const handleAuth = () => {
-    auth
-      .signInWithPopup(provider)
-      .then((result) => {
-        setUser(result.user);
-      })
-      .catch((error) => {
-        alert(error.message);
-      });
+    if (!userName) {
+      auth
+        .signInWithPopup(provider)
+        .then((result) => {
+          setUser(result.user);
+        })
+        .catch((error) => {
+          alert(error.message);
+        });
+    } else if (userName) {
+      auth
+        .signOut()
+        .then(() => {
+          dispatch(setUserLogoutState());
+          history("/");
+        })
+        .catch((error) => alert(error.message));
+    }
   };
   const setUser = (user) => {
     dispatch(
@@ -69,11 +89,16 @@ const Header = (props) => {
               <span>SERIES</span>
             </a>
           </NavMenu>
-          <UserImg
-            src={userPhoto}
-            alt={userPhoto}
-            referrerPolicy="no-referrer"
-          />
+          <SignOut>
+            <UserImg
+              src={userPhoto}
+              alt={userPhoto}
+              referrerPolicy="no-referrer"
+            />
+            <DropDown>
+              <span onClick={handleAuth}>Sign Out</span>
+            </DropDown>
+          </SignOut>
         </>
       )}
     </Nav>
@@ -180,8 +205,39 @@ const Login = styled.a`
 `;
 const UserImg = styled.img`
   height: 100%;
-  border-radius: 40px;
-  margin:10px;
-  padding:5px;
+`;
+const DropDown = styled.div`
+  position: absolute;
+  top: 48px;
+  right: 0px;
+  background: rgb(19, 19, 19);
+  border: 1px solid rgba(151, 151, 151, 0.34);
+  border-radius: 4px;
+  box-shadow: rgba(0 0 0 / 50%) 0px 0px 18px 0px;
+  padding: 10px;
+  font-size: 14px;
+  letter-spacing: 3px;
+  width: 100px;
+  opacity: 0;
+`;
+const SignOut = styled.div`
+  position: relative;
+  height: 48px;
+  width: 48px;
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
+  ${UserImg} {
+    border-radius: 50%;
+    width: 100%;
+    height: 100%;
+  }
+  &:hover {
+    ${DropDown} {
+      opacity: 1;
+      transition-duration: 1s;
+    }
+  }
 `;
 export default Header;
